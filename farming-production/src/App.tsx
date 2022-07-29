db5e86f47a4c0ca5b977089fd783087fd38d65f1
edisonj4vier <rochinaedison75@gmail.com>
2022-07-29 11:02:43 -0500

    c

--- farming-production/src/App.tsx
+++ farming-production/src/App.tsx
@@ -38,24 +38,12 @@ const App: React.FC = () => {
           <Route path="/products" element={<ProductList />} />          
           <Route path="/products/create" element={<ProductForm />} />    
           <Route path="/products/retrieve/:id" element={<ProductCard/>} />      
-<<<<<<< HEAD
           <Route path="/products/update/:id" element={<ProductForm />} />      
-          //Maintenence
+
           <Route path="/maintenance" element={<MaintenenceLsit />} />          
           <Route path="/maintenance/create" element={<MaintenenceForm />} />    
           <Route path="/maintenance/retrieve/:id" element={<MaintenenceCard/>} />      
           <Route path="/maintenance/update/:id" element={<MaintenenceForm />} />     
-=======
-          <Route path="/products/update/:id" element={<ProductForm />} /> 
-
-          {/* Rutas de products */} 
-          <Route path="/products/maintenance/create" element={<MaintenanceForm />} />    
-          <Route path="/products/retrieve/:id" element={<ProductCard/>} />      
-          <Route path="/products/maintenance/update/:id" element={<MaintenanceForm />} />    
-          
-          
-
->>>>>>> 51fe4587ff3e2e71effbb9aad1ffc2101d43c364
         </Routes>
       </div>
     </div>
